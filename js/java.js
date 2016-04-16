b64407a1e41a7a1439853f1308f277fe20740d58
BenBuck123 <Ben@Bens-MacBook-Pro.local>
2016-04-16 09:24:58 -0400

    added new animation

--- js/java.js
+++ js/java.js
@@ -1,9 +1,21 @@
-var main = function() {
-	$(".btn").click(function(){
-    $(".slide-down").toggle();
+$(document).ready(function() {
+	$(window).scroll(function(event) {
+		var y = $(this).scrollTop();
+
+		if (y >= 100) {
+			$('.icons').addClass('animate fadeInLeft')
+		}
+		if (y >= 100) {
+			$('.icons1').addClass('animate fadeInUp')
+		}
+		if (y >= 100) {
+			$('.icons2').addClass('animate fadeInRight')
+		}
+		if (y >= 2533) {
+			$('.excuse').addClass('animate fadeInRight')
+		}
+	});
+
+
+
 });
-
-}
-
-
-$(document).ready(main);
